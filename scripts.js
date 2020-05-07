a81cace712dd9ce773050cf35083d783c08a1251
JEFFGULICK <jeffgulick@gmail.com>
2020-05-06 20:03:19 -0500

    added js back to page

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ const two = () => {
 }
 
 // Four images side by side
-const four =()=> {
+const four = () => {
   for (i = 0; i < elements.length; i++) {
     // elements[i].style.msFlex = "25%";  // IE10
     elements[i].style.flex = "25%";
